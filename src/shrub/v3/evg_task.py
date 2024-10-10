29484e3cbe96c4c524f112c107299bdbd1323bcc
Ezra Chung <ezra.chung@mongodb.com>
2024-10-10 12:34:51 -0500

    Add support for task fields: run_on, disable, and batchtime

--- src/shrub/v3/evg_task.py
+++ src/shrub/v3/evg_task.py
@@ -1,5 +1,5 @@
 """Evergreen models for tasks."""
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import BaseModel
 
@@ -37,18 +37,24 @@ class EvgTask(BaseModel):
     * name: Name of task.
     * commands: List of commands that make up task.
     * depends_on: Other tasks that must be successful for this task to run.
+    * run_on: Distros this task should be run on.
     * exec_timeout_secs: Time task can run before being considered timing out.
     * tags: List of tags to attach to task.
+    * disable: Prevent this task from running at all.
     * patchable: Whether this task can run in patch builds.
+    * batchtime: How frequent this task should be run.
     * stepback: Whether this task should run stepback.
     """
 
     name: str
     commands: Optional[List[EvgCommand]] = None
     depends_on: Optional[List[EvgTaskDependency]] = None
+    run_on: Optional[Union[str, List[str]]] = None
     exec_timeout_secs: Optional[int] = None
     tags: Optional[List[str]] = None
+    disable: Optional[bool] = None
     patchable: Optional[bool] = None
+    batchtime: Optional[int] = None
     stepback: Optional[bool] = None
 
     def get_task_ref(self, distros: Optional[List[str]] = None) -> EvgTaskRef:
