764dee2191020d365693de8a76e52e3d0bb3cffd
Ezra Chung <ezra.chung@mongodb.com>
2024-10-30 11:40:54 -0500

    Address pydantic 2.0 UserWarning for allow_population_by_field_name

--- src/shrub/v3/evg_command.py
+++ src/shrub/v3/evg_command.py
@@ -199,7 +199,7 @@ class BuiltInCommand(BaseModel):
         super().__init__(**kwargs)
 
     class Config:
-        allow_population_by_field_name = True
+        populate_by_name = True
         use_enum_values = True
 
 
